c10bead00bcd1818a2998ac4b99c750bcf3803a2
Monir <ahmadmonirosmani@gmail.com>
2022-11-04 00:05:28 +0100

    initail commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <header>
+      <div class="header__container"></div>
+    </header>
+  );
+}
+
+export default Header;
